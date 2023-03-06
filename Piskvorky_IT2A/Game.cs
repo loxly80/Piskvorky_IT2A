005a5f2d1e6a3828c52a17c870f3e01d22ab9bb2
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-03-06 13:44:30 +0100

    Add project files.

---
+++ Piskvorky_IT2A/Game.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piskvorky_IT2A
+{
+  public class Game
+  {
+    public List<Item> Items { get; } = new List<Item>();
+    public int Top
+    {
+      get
+      {
+        if (Items.Count == 0)
+        {
+          return 0;
+        }
+        else if (Items.Count == 1)
+        {
+          return 5;
+        }
+        else
+        {
+          int top = 0;
+          foreach (Item item in Items)
+          {
+            if(item.Y < top)
+            {
+              top = item.Y;
+            }
+          }
+          return Math.Abs(top) + 5;
+        }
+      }
+    }
+    public int Bottom
+    {
+      get
+      {
+        if (Items.Count == 0)
+        {
+          return 0;
+        }
+        else if (Items.Count == 1)
+        {
+          return 5;
+        }
+        else
+        {
+          int bottom = 0;
+          foreach (Item item in Items)
+          {
+            if (item.Y > bottom)
+            {
+              bottom = item.Y;
+            }
+          }
+          return bottom + 5;
+        }
+      }
+    }
+    public int Left
+    {
+      get
+      {
+        if (Items.Count == 0)
+        {
+          return 0;
+        }
+        else if (Items.Count == 1)
+        {
+          return 5;
+        }
+        else
+        {
+          int left = 0;
+          foreach (Item item in Items)
+          {
+            if (item.X < left)
+            {
+              left = item.Y;
+            }
+          }
+          return Math.Abs(left) + 5;
+        }
+      }
+    }
+  }
+}
---
+++ Piskvorky_IT2A/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piskvorky_IT2A
+{
+  public class Item
+  {
+    public int X { get; }
+    public int Y { get; }
+    public Icon Value { get; }
+
+    public Item(int x, int y, Icon value)
+    {
+      X = x;
+      Y = y;
+      Value = value;
+    }
+
+    public enum Icon
+    {
+      Cross,
+      Circle
+    }
+  }
+
+
+}
