4718e18778a52ee10cb5590ac22059f7d1487cca
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-03-20 13:32:49 +0100

    grid 3x3 napevno - ukázka přechodové výplně

--- Piskvorky_IT2A/MainWindow.xaml.cs
+++ Piskvorky_IT2A/MainWindow.xaml.cs
@@ -23,6 +23,49 @@ namespace Piskvorky_IT2A
     public MainWindow()
     {
       InitializeComponent();
+      Background = Brushes.DarkGray;
+    }
+
+    private void Window_Initialized(object sender, EventArgs e)
+    {
+      Grid grid = new Grid();
+      grid.Width = 150;
+      grid.Height = 150;
+      for (int x = 0; x < 3; x++)
+      {
+        grid.ColumnDefinitions.Add(new ColumnDefinition());
+      }
+      for (int y = 0; y < 3; y++)
+      {
+        grid.RowDefinitions.Add(new RowDefinition());
+      }
+      this.AddChild(grid);
+
+      Button button;
+      for(int x = 0; x < 3; x++)
+      {
+        for(int y = 0; y < 3; y++)
+        {
+          button = new Button();
+          button.Background = Brushes.White;
+          //button.Background = new LinearGradientBrush() 
+          //{ 
+          //  StartPoint=new Point(0,5),
+          //  EndPoint=new Point(10,5),
+          //  GradientStops = new GradientStopCollection() 
+          //  { 
+          //    new GradientStop(Colors.Transparent,0),
+          //    new GradientStop(Colors.White,0.1)
+          //  } 
+          //};
+          button.VerticalAlignment = VerticalAlignment.Stretch;
+          button.HorizontalAlignment = HorizontalAlignment.Stretch;
+          Grid.SetColumn(button, x);
+          Grid.SetRow(button, y);
+          grid.Children.Add(button);
+        }
+      }
+
     }
   }
 }
